4735c692feb31ac545e0a31857276cfb54a5d953
Renier <renier@equalily.com>
2019-03-14 21:49:25 +0200

    Tweaks

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -12,5 +12,5 @@ php -dallow_url_fopen=1 composer.phar self-update
 
 php -dallow_url_fopen=1 composer.phar install --prefer-dist --no-dev
 
-cp -r /usr/home/{{ $account }}/source/public/. /usr/home/{{ $account }}/public_html
+cp -r /usr/home/{{ $account }}/source/public/. /usr/home/{{ $account }}/public_html/schoolbase/
 @endtask
