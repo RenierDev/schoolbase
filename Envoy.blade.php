4e773f2f1bfc552ab5e75fa94ca0abc3f53b4317
Renier <renier@equalily.com>
2019-03-14 21:41:58 +0200

    added Envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,16 @@
+@servers(['hetzner' => 'equalayaek@equalily.com'])
+@setup
+$account = 'equalayaek';
+@endsetup
+
+@task('deploy:production', ['on' => 'hetzner'])
+cd /usr/home/{{ $account }}/source
+
+git pull origin master --force
+
+php -dallow_url_fopen=1 composer.phar self-update
+
+php -dallow_url_fopen=1 composer.phar install --prefer-dist --no-dev
+
+cp -r /usr/home/{{ $account }}/source/public/. /usr/home/{{ $account }}/public_html
+@endtask
